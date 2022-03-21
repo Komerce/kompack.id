9c891da2cbcfbfbab16c10e178ca04882636ac2d
IR_Diky <imamdikiridamwan@gmail.com>
2022-03-21 10:29:28 +0700

    modified App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,31 @@
 <template>
   <div id="app">
+    <Header />
     <router-view></router-view>
+    <Footer />
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
+    Header,
+    Footer
   }
 }
 </script>
 
 <style>
-
+  #app {
+    font-family: 'Poppins', "public sans", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    margin-top: 0px;
+    max-width: 100%;
+    background-color: #F8F8F8;
+  }
 </style>
